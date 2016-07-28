5195459984a04749f0efec5d4b6897fd5a322396
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-28 16:32:00 -0400

    thinking

--- src/LN/UI/Core/App.hs
+++ src/LN/UI/Core/App.hs
@@ -1,14 +1,28 @@
+{-# LANGUAGE LambdaCase #-}
+
 module LN.UI.Core.App (
   runCore
 ) where
 
 
-import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.IO.Class   (MonadIO)
+import           Control.Monad.RWS.Strict
 import           Data.Tuple.Select
+import           Haskell.Api.Helpers
 
 import           LN.UI.Core.Control
+import           LN.UI.Core.Router
+import           LN.UI.Core.State
 
 
 
 runCore :: MonadIO m => CoreState -> m CoreState
 runCore st = sel2 <$> runCoreM st (pure ())
+
+
+
+runRoute :: MonadIO m => CoreM m ()
+runRoute = do
+  gets _route >>= \case
+    RouteWith Home _ -> pure ()
+    RouteWith _ _    -> pure ()
