f38b55af1c88b4dee04dc196d26fe7b5efbcd290
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-28 04:12:39 -0400

    works, kinda

--- src/LN/UI/Core/Access.hs
+++ src/LN/UI/Core/Access.hs
@@ -5,6 +5,9 @@
 --
 
 module LN.UI.Core.Access (
+  HasAccess,
+  open,
+  empty,
   isMemberOfOrganization,
   isMemberOfOrganizationHTML,
   isMemberOfOrganizationHTML',
@@ -30,6 +33,12 @@ import           LN.UI.Core.Types
 
 
 
+class HasAccess a where
+  open :: a -> a
+  empty :: a
+
+
+
 -- | Check whether a user is a member of Team_Members
 --
 isMemberOfOrganization :: OrganizationPackResponse -> Bool
@@ -40,10 +49,11 @@ isMemberOfOrganization OrganizationPackResponse{..} = Team_Members `elem` organi
 -- | Supports handlers for if a user is/isnt a member
 --
 isMemberOfOrganizationHTML
-  :: OrganizationPackResponse
-  -> View_
-  -> View_
-  -> View_
+  :: forall m. HasAccess m
+  => OrganizationPackResponse
+  -> m
+  -> m
+  -> m
 
 isMemberOfOrganizationHTML pack is_member_handler isnt_member_handler =
   if isMemberOfOrganization pack
@@ -55,94 +65,96 @@ isMemberOfOrganizationHTML pack is_member_handler isnt_member_handler =
 -- | Supports a handler if a user is a member
 --
 isMemberOfOrganizationHTML'
-  :: OrganizationPackResponse
-  -> View_
-  -> View_
+  :: forall m. HasAccess m
+  => OrganizationPackResponse
+  -> m
+  -> m
 
 isMemberOfOrganizationHTML' pack is_member_handler =
   if isMemberOfOrganization pack
     then is_member_handler
-    else mempty
+    else empty
 
 
 
 permissionsHTML
-  :: Permissions
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-permissionsHTML perms create_cb no_create_cb read_cb no_read_cb update_cb no_update_cb delete_cb no_delete_cb execute_cb no_execute_cb =
-  mconcat
-    [
-      if Perm_Create  `elem` perms then create_cb  else no_create_cb
-    , if Perm_Read    `elem` perms then read_cb    else no_read_cb
-    , if Perm_Update  `elem` perms then update_cb  else no_update_cb
-    , if Perm_Delete  `elem` perms then delete_cb  else no_delete_cb
-    , if Perm_Execute `elem` perms then execute_cb else no_execute_cb
-    ]
+  :: forall m. HasAccess m
+  => Permissions
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+permissionsHTML perms create_cb no_create_cb read_cb no_read_cb update_cb no_update_cb delete_cb no_delete_cb execute_cb no_execute_cb = do
+  open $ do
+    if Perm_Create  `elem` perms then create_cb  else no_create_cb
+    --  if Perm_Read    `elem` perms then read_cb    else no_read_cb
+    -- if Perm_Update  `elem` perms then update_cb  else no_update_cb
+    -- if Perm_Delete  `elem` perms then delete_cb  else no_delete_cb
+    -- if Perm_Execute `elem` perms then execute_cb else no_execute_cb
 
 
 
 permissionsHTML'
-  :: Permissions
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
-  -> View_
+  :: forall m. HasAccess m
+  => Permissions
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
+  -> m
 permissionsHTML' perms create_cb read_cb update_cb delete_cb execute_cb =
-  permissionsHTML perms create_cb mempty read_cb mempty update_cb mempty delete_cb mempty execute_cb mempty
+  permissionsHTML perms create_cb empty read_cb empty update_cb empty delete_cb empty execute_cb empty
 
 
 
-permCreateEmpty, permReadEmpty, permUpdateEmpty, permDeleteEmpty, permExecuteEmpty :: View_
-permCreateEmpty  = mempty
-permReadEmpty    = mempty
-permUpdateEmpty  = mempty
-permDeleteEmpty  = mempty
-permExecuteEmpty = mempty
+permCreateEmpty, permReadEmpty, permUpdateEmpty, permDeleteEmpty, permExecuteEmpty :: forall m. HasAccess m => m
+permCreateEmpty  = empty
+permReadEmpty    = empty
+permUpdateEmpty  = empty
+permDeleteEmpty  = empty
+permExecuteEmpty = empty
 
 
 
-permissionsMatchCreateHTML :: Permissions -> View_ -> View_ -> View_
+permissionsMatchCreateHTML :: forall m. HasAccess m => Permissions -> m -> m -> m
 permissionsMatchCreateHTML = permissionsMatchHTML Perm_Create
 
 
 
-permissionsMatchReadHTML :: Permissions -> View_ -> View_ -> View_
+permissionsMatchReadHTML :: forall m. HasAccess m => Permissions -> m -> m -> m
 permissionsMatchReadHTML = permissionsMatchHTML Perm_Read
 
 
 
-permissionsMatchUpdateHTML :: Permissions -> View_ -> View_ -> View_
+permissionsMatchUpdateHTML :: forall m. HasAccess m => Permissions -> m -> m -> m
 permissionsMatchUpdateHTML = permissionsMatchHTML Perm_Update
 
 
 
-permissionsMatchDeleteHTML :: Permissions -> View_ -> View_ -> View_
+permissionsMatchDeleteHTML :: forall m. HasAccess m => Permissions -> m -> m -> m
 permissionsMatchDeleteHTML = permissionsMatchHTML Perm_Delete
 
 
 
-permissionsMatchExecuteHTML :: Permissions -> View_ -> View_ -> View_
+permissionsMatchExecuteHTML :: forall m. HasAccess m => Permissions -> m -> m -> m
 permissionsMatchExecuteHTML = permissionsMatchHTML Perm_Execute
 
 
 
 permissionsMatchHTML
-  :: Permission
+  :: forall m. HasAccess m
+  => Permission
   -> Permissions
-  -> View_
-  -> View_
-  -> View_
+  -> m
+  -> m
+  -> m
 permissionsMatchHTML perm_to_match permissions is_match_handler isnt_match_handler =
   if perm_to_match `elem` permissions then is_match_handler else isnt_match_handler
