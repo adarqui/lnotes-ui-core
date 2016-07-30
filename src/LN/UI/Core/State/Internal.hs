113d2f95e3349255f64d425938afba6660f55405
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-30 03:40:43 -0400

    checkpoint

--- src/LN/UI/Core/State/Internal.hs
+++ src/LN/UI/Core/State/Internal.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE BangPatterns      #-}
 
 module LN.UI.Core.State.Internal (
   Store (..),
@@ -32,28 +33,28 @@ import           LN.UI.Core.Types
 
 
 data Store = Store {
-      _route                   :: RouteWith
-    , _pageInfo                :: PageInfo
-    , _l_m_me                  :: Loader (Maybe UserResponse)
-    , _meId                    :: UserId
-    , _usersCache              :: Map UserId UserSanitizedPackResponse
-    , _l_organizations         :: Loader (Map OrganizationId OrganizationPackResponse)
-    , _l_users                 :: Loader (Map UserId UserSanitizedPackResponse)
-    , _l_forums                :: Loader (Map ForumId ForumPackResponse)
-    , _l_boards                :: Loader (Map BoardId BoardPackResponse)
-    , _l_threads               :: Loader (Map ThreadId ThreadPackResponse)
-    , _l_posts                 :: Loader (Map ThreadId ThreadPostPackResponse)
-    , _l_m_organization        :: Loader (Maybe OrganizationPackResponse)
-    , _m_organizationRequest   :: Maybe OrganizationRequest
-    , _l_m_user                :: Loader (Maybe UserPackResponse)
-    , _l_m_forum               :: Loader (Maybe ForumPackResponse)
-    , _m_forumRequest          :: Maybe ForumRequest
-    , _l_m_board               :: Loader (Maybe BoardPackResponse)
-    , _m_boardRequest          :: Maybe BoardRequest
-    , _l_m_thread              :: Loader (Maybe ThreadPackResponse)
-    , _m_threadRequest         :: Maybe ThreadRequest
-    , _l_m_threadPost          :: Loader (Maybe ThreadPostPackResponse)
-    , _m_threadPostRequest     :: Maybe ThreadPostRequest
+      _route                   :: !RouteWith
+    , _pageInfo                :: !PageInfo
+    , _l_m_me                  :: !(Loader (Maybe UserResponse))
+    , _meId                    :: !UserId
+    , _usersCache              :: !(Map UserId UserSanitizedPackResponse)
+    , _l_organizations         :: !(Loader (Map OrganizationId OrganizationPackResponse))
+    , _l_users                 :: !(Loader (Map UserId UserSanitizedPackResponse))
+    , _l_forums                :: !(Loader (Map ForumId ForumPackResponse))
+    , _l_boards                :: !(Loader (Map BoardId BoardPackResponse))
+    , _l_threads               :: !(Loader (Map ThreadId ThreadPackResponse))
+    , _l_posts                 :: !(Loader (Map ThreadId ThreadPostPackResponse))
+    , _l_m_organization        :: !(Loader (Maybe OrganizationPackResponse))
+    , _m_organizationRequest   :: !(Maybe OrganizationRequest)
+    , _l_m_user                :: !(Loader (Maybe UserPackResponse))
+    , _l_m_forum               :: !(Loader (Maybe ForumPackResponse))
+    , _m_forumRequest          :: !(Maybe ForumRequest)
+    , _l_m_board               :: !(Loader (Maybe BoardPackResponse))
+    , _m_boardRequest          :: !(Maybe BoardRequest)
+    , _l_m_thread              :: !(Loader (Maybe ThreadPackResponse))
+    , _m_threadRequest         :: !(Maybe ThreadRequest)
+    , _l_m_threadPost          :: !(Loader (Maybe ThreadPostPackResponse))
+    , _m_threadPostRequest     :: !(Maybe ThreadPostRequest)
     } deriving (Typeable, Generic, NFData)
 
 
