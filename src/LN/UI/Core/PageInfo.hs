df1983c306ab194d9e3ab98b22a65ace9f8e693c
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-11 01:47:52 -0400

    cleanup

--- src/LN/UI/Core/PageInfo.hs
+++ src/LN/UI/Core/PageInfo.hs
@@ -8,17 +8,6 @@ module LN.UI.Core.PageInfo (
   pageInfoFromParams,
   paramsFromPageInfo,
   runPageInfo
-  -- defaultPageInfo,
-  -- defaultPageInfo_Users,
-  -- defaultPageInfo_Organizations,
-  -- defaultPageInfo_Forums,
-  -- defaultPageInfo_Threads,
-  -- defaultPageInfo_ThreadPosts,
-  -- defaultPageInfo_Resources,
-  -- defaultPageInfo_Leurons,
-  -- defaultPageInfo_Workouts,
-  -- RunPageInfo,
-  -- runPageInfo
 ) where
 
 
@@ -117,86 +106,3 @@ runPageInfo CountResponses{..} page_info =
         totalPages   = (countResponseN `div` resultsPerPage page_info)
       }
     _      -> page_info
-
-
-
--- defaultPageInfo_Users :: PageInfo
--- defaultPageInfo_Users = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- defaultPageInfo_Organizations :: PageInfo
--- defaultPageInfo_Organizations = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- defaultPageInfo_Forums :: PageInfo
--- defaultPageInfo_Forums = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- defaultPageInfo_Boards :: PageInfo
--- defaultPageInfo_Boards = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- defaultPageInfo_Threads :: PageInfo
--- defaultPageInfo_Threads = defaultPageInfo { sortOrder = SortOrderBy_Dsc, order = OrderBy_ActivityAt }
-
-
-
--- defaultPageInfo_ThreadPosts :: PageInfo
--- defaultPageInfo_ThreadPosts = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- defaultPageInfo_Resources :: PageInfo
--- defaultPageInfo_Resources = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- defaultPageInfo_Leurons :: PageInfo
--- defaultPageInfo_Leurons = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- defaultPageInfo_Workouts :: PageInfo
--- defaultPageInfo_Workouts = defaultPageInfo -- { orderBy = OrderBy_CreatedAt }
-
-
-
--- -- For page numbers stuff, inside Eval components
-
-
--- type RunPageInfo = {
---   count    :: Int,
---   pageInfo :: PageInfo,
---   params   :: Array Param
--- }
-
-
-
--- runPageInfo :: CountResponses -> PageInfo -> RunPageInfo
--- runPageInfo count_responses page_info =
---   {
---     count: count,
---     pageInfo: pi,
---     params: par
---   }
---   where
---   count =
---     maybe
---       0
---       (\count_response -> count_response ^. _CountResponse .. n_)
---       (head (count_responses ^. _CountResponses .. countResponses_))
---   pi  =
---     page_info {
---       totalResults = count,
---       totalPages   = (count / page_info.resultsPerPage) + 1
---     }
---   par =
---     [ Limit page_info.resultsPerPage
---     , Offset ((page_info.currentPage - 1) * page_info.resultsPerPage)
---     , SortOrder page_info.sortOrder
---     , Order page_info.order
---     ]
