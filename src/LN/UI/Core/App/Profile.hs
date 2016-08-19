4573c1eec0f42d56c982d9d4e19ce9590e1deb94
Andrew Darqui <andrew.darqui@gmail.com>
2016-08-19 00:06:20 -0400

    fix: need bang patterns, strictness mandatory

--- src/LN/UI/Core/App/Profile.hs
+++ src/LN/UI/Core/App/Profile.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns    #-}
 {-# LANGUAGE ExplicitForAll  #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -26,8 +27,8 @@ import           LN.UI.Core.State
 
 
 setGender :: ProfileRequest -> ProfileGender -> Action
-setGender request@ProfileRequest{..} input =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestGender = input}})
+setGender !request@ProfileRequest{..} !input =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestGender = input}})
 
 
 
@@ -37,53 +38,53 @@ setBirthdate _ _ = undefined
 
 
 setWebsite :: ProfileRequest -> Text -> Action
-setWebsite request@ProfileRequest{..} input =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestWebsite = Just input}})
+setWebsite !request@ProfileRequest{..} !input =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestWebsite = Just input}})
 
 
 
 clearWebsite :: ProfileRequest -> Action
-clearWebsite request@ProfileRequest{..} =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestWebsite = Nothing}})
+clearWebsite !request@ProfileRequest{..} =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestWebsite = Nothing}})
 
 
 
 setWebsites :: ProfileRequest -> [Text] -> Action
-setWebsites request@ProfileRequest{..} input =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestWebsites = input}})
+setWebsites !request@ProfileRequest{..} !input =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestWebsites = input}})
 
 
 
 clearWebsites :: ProfileRequest -> Action
-clearWebsites request@ProfileRequest{..} =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestWebsites = []}})
+clearWebsites !request@ProfileRequest{..} =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestWebsites = []}})
 
 
 
 setLocation :: ProfileRequest -> Text -> Action
-setLocation request@ProfileRequest{..} input =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestLocation = Just input}})
+setLocation !request@ProfileRequest{..} !input =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestLocation = Just input}})
 
 
 
 clearLocation :: ProfileRequest -> Action
-clearLocation request@ProfileRequest{..} =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestLocation = Nothing}})
+clearLocation !request@ProfileRequest{..} =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestLocation = Nothing}})
 
 
 
 setSignature :: ProfileRequest -> Text -> Action
-setSignature request@ProfileRequest{..} input =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestSignature = Just input}})
+setSignature !request@ProfileRequest{..} !input =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestSignature = Just input}})
 
 
 
 clearSignature :: ProfileRequest -> Action
-clearSignature request@ProfileRequest{..} =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestSignature = Nothing}})
+clearSignature !request@ProfileRequest{..} =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestSignature = Nothing}})
 
 
 
 setDebug :: ProfileRequest -> Bool -> Action
-setDebug request@ProfileRequest{..} input =
-  ApplyState (\st->st{_m_profileRequest = Just $! request{profileRequestDebug = input}})
+setDebug !request@ProfileRequest{..} !input =
+  ApplyState (\st->st{_m_profileRequest = Just $ request{profileRequestDebug = input}})
